09f3464c06e880d887b1fa80702838fd5a68315c
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-05-29 14:25:07 +0545

    Modified SqlBuilderExceptionHandler src func().

--- handler/SqlBuilderExceptionHandler.php
+++ handler/SqlBuilderExceptionHandler.php
@@ -2,60 +2,63 @@
 
 namespace NGFramer\NGFramerPHPException\handler;
 
-use Exception;
+use Throwable;
 use NGFramer\NGFramerPHPException\exception\SqlBuilderException;
 
 class SqlBuilderExceptionHandler
 {
-    public static function handle(Exception $exception)
+    public static function handle(Throwable $exception)
     {
+        // Get the message, statusCode, and also the errorDetails.
         $errorMessage = $exception->getMessage();
         $statusCode = ($exception instanceof SqlBuilderException) ? $exception->getStatusCode() : 500;
         $errorDetails = ($exception instanceof SqlBuilderException) ? $exception->getErrorDetails() : [];
-        $source = self::getSource();
+
+        // Use the passed backtrace if available.
+        $source = isset($errorDetails['backtrace']) ? self::getSource($errorDetails['backtrace']) : null;
 
         http_response_code($statusCode);
         echo json_encode(
             [
                 'success' => false,
                 'status_code' => $statusCode,
-                'source' => $source,
                 'response' =>
                     [
                         'error_type' => $errorDetails['error_type'] ?? $errorDetails[0] ?? 'UNDEFINED',
                         'error_code' => $errorDetails['error_code'] ?? $errorDetails[1] ?? 'UNDEFINED',
                         'error_message' => $errorMessage,
-                    ]
+                    ],
+                'source' => $source
             ]
         );
         exit;
     }
 
-    public static function getSource(): array
+
+    public static function getSource($backtrace = null): array
     {
-        // Get the trace.
-        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
+        // Use the provided backtrace if available.
+        if (empty($backtrace)) $traces = $backtrace;
+        else $traces[] = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
 
-        // Check if called from within a class method
-        $info = $trace[1];
-        $namespace = '';
-        $class = $info['class'] ?? '';
-        $function = $info['function'] ?? '';
-        $line = $trace[0]['line'] ?? '';
+        // Initialize the trace map.
+        $traceMap = [];
 
-        // Extract namespace if class name includes it
-        if ($class) {
-            $namespaceParts = explode('\\', $class);
-            $class = array_pop($namespaceParts);
-            $namespace = implode('\\', $namespaceParts);
+        // Loop through to get the trace elements.
+        foreach ($traces as $trace) {
+            // Start joining the trace elements.
+            $joiningString = $trace['type'] ?? ">>";
+            $currentTrace = $trace['file'] . $joiningString ?? '';
+            $currentTrace .= $trace['class'] . $joiningString ?? '';
+            $currentTrace .= $trace['object'] . $joiningString ?? '';
+            $currentTrace .= $trace['function'] . $joiningString ?? '';
+            $currentTrace .= $trace['line'] . $joiningString ?? '';
+
+            // Add the current trace to the trace map.
+            $traceMap[] = $currentTrace;
         }
 
-        return [
-            'namespace' => $namespace,
-            'class' => $class,
-            'function' => $function,
-            'line' => $line
-        ];
+        // Return the trace collection
+        return $traceMap;
     }
-
 }
