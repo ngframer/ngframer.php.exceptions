b814f3bc84da8a772ee69db33fe084ff52aa3580
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-05-29 14:30:24 +0545

    Created SqlBuilderError exception.
    Converts the error to SqlBuilderException, is also an /Error class.

---
+++ exception/SqlBuilderError.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace NGFramer\NGFramerPHPException\exception;
+
+use Error;
+
+class SqlBuilderError extends Error
+{
+    public static function convertToException(int $errorCode, string $errorMessage, string $errorFile, int $errorLine): void
+    {
+        // Error type and code defined.
+        $errorDetails['error_type'] = error_get_last()['type'] ?? 'UNDEFINED';
+        $errorDetails['error_code'] = $errorCode;
+
+        // Error file and line defined for debugging.
+        $errorDetails['file'] = $errorFile;
+        $errorDetails['line'] = $errorLine;
+
+        // Error backtrace defined for debugging.
+        $errorDetails['backtrace'] = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+
+        // Throw the exception (SqlBuilderException).
+        throw new SqlBuilderException($errorMessage, 500, $errorDetails);
+    }
+}
