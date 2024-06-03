1d6779400a26d8797d2425fc9de810ee8735e252
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-03 20:04:44 +0545

    Refactored getSource function to trait.

--- handlers/SqlBuilderExceptionHandler.php
+++ handlers/SqlBuilderExceptionHandler.php
@@ -4,9 +4,15 @@ namespace NGFramer\NGFramerPHPExceptions\handlers;
 
 use Throwable;
 use NGFramer\NGFramerPHPExceptions\exceptions\SqlBuilderException;
+use NGFramer\NGFramerPHPExceptions\handlers\supportive\SourceTrait;
 
 class SqlBuilderExceptionHandler
 {
+    // Traits used in this class.
+    use SourceTrait;
+
+
+    // Main handler function for the SqlBuilderException.
     public static function handle(Throwable $exception)
     {
         // Get the message, statusCode, and also the errorDetails.
@@ -32,33 +38,5 @@ class SqlBuilderExceptionHandler
             ]
         );
         exit;
-    }
-
-
-    public static function getSource($backtrace = null): array
-    {
-        // Use the provided backtrace if available.
-        if (empty($backtrace)) $traces = $backtrace;
-        else $traces[] = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-
-        // Initialize the trace map.
-        $traceMap = [];
-
-        // Loop through to get the trace elements.
-        foreach ($traces as $trace) {
-            // Start joining the trace elements.
-            $joiningString = $trace['type'] ?? ">>";
-            $currentTrace = isset($trace['file']) ? $trace['file'] . $joiningString : '';
-            $currentTrace .= isset($trace['class']) ? $trace['class'] . $joiningString : '';
-            $currentTrace .= isset($trace['object']) ? $trace['object'] . $joiningString : '';
-            $currentTrace .= isset($trace['function']) ? $trace['function'] . $joiningString : '';
-            $currentTrace .= isset($trace['line']) ? $trace['line'] . $joiningString : '';
-
-            // Add the current trace to the trace map.
-            $traceMap[] = $currentTrace;
-        }
-
-        // Return the trace collection
-        return $traceMap;
     }
 }
