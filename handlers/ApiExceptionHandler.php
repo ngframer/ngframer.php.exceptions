99d28ee10b2aa6bc1b41da93ad7457e6e223e963
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-03 22:56:29 +0545

    Created ApiExceptionHandler class.
    The ApiExceptionHandler is consistent to the other handlers developed.

---
+++ handlers/ApiExceptionHandler.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace NGFramer\NGFramerPHPExceptions\handlers;
+
+use NGFramer\NGFramerPHPExceptions\exceptions\ApiException;
+use NGFramer\NGFramerPHPExceptions\handlers\supportive\SourceTrait;
+use Throwable;
+
+class ApiExceptionHandler
+{
+    // Traits for the ApiExceptionHandler.
+    use SourceTrait;
+
+
+    // Handle the exception, handle function.
+    public static function handle(Throwable $exception): void
+    {
+        // Get the message, statusCode, and also the details.
+        $message = $exception->getMessage();
+        $code = $exception->getCode();
+        $statusCode = ($exception instanceof ApiException) ? $exception->getStatusCode() : 500;
+        $details = ($exception instanceof ApiException) ? $exception->getDetails() : [];
+
+        // Use the passed backtrace if available.
+        $source = isset($details['backtrace']) ? self::getSource($details['backtrace']) : null;
+
+        // Define the response to throw as method of error handling.
+        $response = [
+            'success' => false,
+            'status_code' => $statusCode,
+            'response' => [
+                'error_message' => $message,
+                'error_code' => $code,
+                'error_type' => $details['error_type'] ?? $details[0] ?? 'UNDEFINED',
+            ]
+        ];
+
+        // If source is available, add it to the response.
+        if (APPMODE === 'development') {
+            $response['source'] = $source;
+        }
+
+        // Set the status code and echo the response.
+        http_response_code($statusCode);
+        echo json_encode($response);
+
+        // Exit the application.
+        exit;
+    }
+}
