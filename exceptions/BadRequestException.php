87beda3d4b412718fec2abf15d8ec92cee5a8a83
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-03 20:48:35 +0545

    Updated BadReqException relative to BaseException.

--- exceptions/BadRequestException.php
+++ exceptions/BadRequestException.php
@@ -2,10 +2,14 @@
 
 namespace NGFramer\NGFramerPHPExceptions\exceptions;
 
-use Exception;
+use NGFramer\NGFramerPHPExceptions\exceptions\supportive\_BaseException;
 
-class BadRequestException extends Exception
+class BadRequestException extends _BaseException
 {
-	protected $code = 400;
+    // Updated the values of this class.
 	protected $message = "The request is invalid.";
+    // TODO: Change the code based on the documentation in the upcoming time.
+	protected $code = 0;
+    protected int $statusCode = 400;
+    protected array $details = [];
 }
