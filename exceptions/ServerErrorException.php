8e3793fcc4d0e66136e6d13b83d1616c1e7ff2fc
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-03 20:53:42 +0545

    Updated ServerErrorExpt as per BaseExpt.

--- exceptions/ServerErrorException.php
+++ exceptions/ServerErrorException.php
@@ -2,10 +2,14 @@
 
 namespace NGFramer\NGFramerPHPExceptions\exceptions;
 
-use Exception;
+use NGFramer\NGFramerPHPExceptions\exceptions\supportive\_BaseException;
 
-class ServerErrorException extends Exception
+class ServerErrorException extends _BaseException
 {
-    protected $code = 500;
+    // Updated the values of this class.
     protected $message = "Server Error";
+    // TODO: Change the code based on the documentation in the upcoming time.
+    protected $code = 0;
+    protected int $statusCode = 500;
+    protected array $details = [];
 }
