70492c7ce841e903db68e3bbe9c1c2af10baab0a
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-04 16:14:54 +0545

    Refactored SqlBuilderExptHndlr, uses ApiExptHndlr.

--- handlers/SqlBuilderExceptionHandler.php
+++ handlers/SqlBuilderExceptionHandler.php
@@ -2,49 +2,6 @@
 
 namespace NGFramer\NGFramerPHPExceptions\handlers;
 
-use NGFramer\NGFramerPHPExceptions\exceptions\SqlBuilderException;
-use NGFramer\NGFramerPHPExceptions\handlers\supportive\SourceTrait;
-use Throwable;
-
-class SqlBuilderExceptionHandler
+class SqlBuilderExceptionHandler extends ApiExceptionHandler
 {
-    // Traits for the SqlBuilderExceptionHandler.
-    use SourceTrait;
-
-
-    // Handle the exception, handle function.
-    public static function handle(Throwable $exception): void
-    {
-        // Get the message, statusCode, and also the details.
-        $message = $exception->getMessage();
-        $code = $exception->getCode();
-        $statusCode = ($exception instanceof SqlBuilderException) ? $exception->getStatusCode() : 500;
-        $details = ($exception instanceof SqlBuilderException) ? $exception->getDetails() : [];
-
-        // Use the passed backtrace if available.
-        $source = isset($details['backtrace']) ? self::getSource($details['backtrace']) : null;
-
-        // Define the response to throw as method of error handling.
-        $response = [
-            'success' => false,
-            'status_code' => $statusCode,
-            'response' => [
-                'error_message' => $message,
-                'error_code' => $code,
-                'error_type' => $details['error_type'] ?? $details[0] ?? 'UNDEFINED',
-            ]
-        ];
-
-        // If source is available, add it to the response.
-        if (APPMODE === 'development') {
-            $response['source'] = $source;
-        }
-
-        // Set the status code and echo the response.
-        http_response_code($statusCode);
-        echo json_encode($response);
-
-        // Exit the application.
-        exit;
-    }
 }
