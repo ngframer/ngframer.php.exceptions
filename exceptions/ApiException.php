b75b233f6e3b8634d0c500972e4158ee7ba228df
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-16 16:50:58 +0545

    Refactored ApiException to remove details.
    The BaseException automatically defines the detailed trace now.

--- exceptions/ApiException.php
+++ exceptions/ApiException.php
@@ -10,12 +10,7 @@ class ApiException extends _BaseException
     // Constructor for the ApiException.
     public function __construct($message, $code = 0, ?Throwable $previous = null, int $statusCode = 500, array $details = [])
     {
-        // Firstly we go for backtrace.
-        // Get the error details and update it.
-        // We check if it has been set, as this class is also used by another class, and they can pass the backtrace directly.
-        $details['backtrace'] = $details['backtrace'] ?? debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-
-        // Call the parent constructor for exception.
+        // Call the parent constructor  for exception.
         parent::__construct($message, $code, $previous, $statusCode, $details);
     }
 }
