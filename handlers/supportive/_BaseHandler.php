d40943f9586d3af1dfd20b9d959924b53a2abd1f
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-16 16:27:58 +0545

    Created a base handler class for the exceptions.
    Uses the values provided by the Exception (not custom) at first, and then checks and uses the values from custom exceptions if needed.
    
    Follows the BaseException class for custom Exception handelling.

---
+++ handlers/supportive/_BaseHandler.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace NGFramer\NGFramerPHPExceptions\handlers\supportive;
+
+use NGFramer\NGFramerPHPExceptions\config\ApplicationConfig;
+use NGFramer\NGFramerPHPExceptions\exceptions\supportive\_BaseException;
+use Throwable;
+
+abstract class _BaseHandler
+{
+    // Variable used across this and other extending classes to get the responses.
+    protected array $response;
+
+
+    // Handle the exception, handle function.
+    public function handle(Throwable $exception): void
+    {
+        // Get the statusCode and the details.
+        $statusCode = ($exception instanceof _BaseException) ? $exception->getStatusCode() : 500;
+        $details = ($exception instanceof _BaseException) ? $exception->getDetails() : [];
+
+        // Operations on the file and line of error to show for errorSource as string.
+        $joinString = " :";
+        $errorSource = $exception->getFile() . $joinString . $exception->getLine();
+
+        // Operations on the traces of error to show as string.
+        $errorTrace = [];
+        foreach ($exception->getTrace() as $indivTrace) {
+            $errorTrace[] = $indivTrace['file'] . $joinString . $indivTrace['line'] . $joinString . $indivTrace['function'] . $joinString . json_encode($indivTrace['args']) ?? "";
+        }
+
+        // Define the response to throw as method of error handling.
+        $response = [
+            'success' => false,
+            'statusCode' => $statusCode,
+            'details' => [
+                // Error code is the 2nd parameter of Exception ($code = 0).
+                'errorCode' => $exception->getCode(),
+                // Error type is defined by the user is string, defaults to .
+                'errorType' => $details['errorType'] ?? 'undefined',
+                // Error message is the 1st parameter of Exception ($message = "").
+                'errorMessage' => $exception->getMessage()
+            ]
+        ];
+
+        // Only available for the developer, data to debug.
+        $appMode = ApplicationConfig::get('appMode');
+        if ($appMode === 'development') {
+            $response['details']['errorSource'] = $errorSource;
+            $response['details']['errorTrace'] = $errorTrace;
+            if (!empty($exception->getPrevious())) {
+                $response['previous'] = $exception->getPrevious();
+            }
+        }
+
+        // Save the response to the class base.
+        $this->response = $response;
+    }
+}
