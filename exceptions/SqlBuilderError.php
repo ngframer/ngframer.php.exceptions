5356804ebdbc79521bcdb819898aab546e069c68
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-16 16:58:46 +0545

    Removed detail generation from SqlBuilderError.
    Will be automatically generated by the Exception and if not by the handler.

--- exceptions/SqlBuilderError.php
+++ exceptions/SqlBuilderError.php
@@ -8,10 +8,7 @@ class SqlBuilderError extends _BaseError
 {
     public function convertToException($code, $message, string $file, int $line, array $context = []): void
     {
-        // Call the parent's method.
-        $details = parent::generateDetails($file, $line, $context);
-
         // Throw the exception (ApiError).
-        throw new SqlBuilderException($message, $code, null, 500, $details);
+        throw new SqlBuilderException($message, $code, null, 500, []);
     }
 }
