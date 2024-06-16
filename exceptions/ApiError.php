99b6fff6cb44cb90b4ac40c27b7743c4c0ff4931
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-16 16:37:44 +0545

    Refactored to remove details from ApiError.

--- exceptions/ApiError.php
+++ exceptions/ApiError.php
@@ -8,10 +8,7 @@ class ApiError extends _BaseError
 {
     public function convertToException($code, $message, string $file, int $line, array $context = []): void
     {
-        // Call the parent's method.
-        $details = parent::generateDetails($file, $line, $context);
-
         // Throw the exception (ApiError).
-        throw new ApiException($message, $code, null, 500, $details);
+        throw new ApiException($message, $code, null, 500, []);
     }
 }
