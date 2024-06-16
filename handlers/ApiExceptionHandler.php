07382e9ebee2dd7bba328c930acf6ab53d480bd7
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-16 16:30:06 +0545

    Refactored ApiHandler Exception as per BaseException.

--- handlers/ApiExceptionHandler.php
+++ handlers/ApiExceptionHandler.php
@@ -2,53 +2,18 @@
 
 namespace NGFramer\NGFramerPHPExceptions\handlers;
 
-use app\config\ApplicationConfig;
-use NGFramer\NGFramerPHPExceptions\exceptions\supportive\_BaseException;
-use NGFramer\NGFramerPHPExceptions\handlers\supportive\SourceTrait;
+use NGFramer\NGFramerPHPExceptions\handlers\supportive\_BaseHandler;
 use Throwable;
 
-class ApiExceptionHandler
+class ApiExceptionHandler extends _BaseHandler
 {
-    // Traits for the ApiExceptionHandler.
-    use SourceTrait;
-
-
-    // Handle the exception, handle function.
-    public static function handle(Throwable $exception): void
+    public function handle(Throwable $exception): void
     {
-        // Get the message, statusCode, and also the details.
-        $message = $exception->getMessage();
-        $code = $exception->getCode();
-        $previous = $exception->getPrevious();
-        $statusCode = ($exception instanceof _BaseException) ? $exception->getStatusCode() : 500;
-        $details = ($exception instanceof _BaseException) ? $exception->getDetails() : [];
-
-        // Use the passed backtrace if available.
-        $source = isset($details['backtrace']) ? self::getSource($details['backtrace']) : null;
-
-        // Define the response to throw as method of error handling.
-        $response = [
-            'success' => false,
-            'status_code' => $statusCode,
-            'response' => [
-                'error_message' => $message,
-                'error_code' => $code,
-                'error_type' => $details['error_type'] ?? $details[0] ?? 'UNDEFINED',
-            ]
-        ];
-
-        // If source is available, add it to the response.
-        $appMode = ApplicationConfig::get('appMode');
-        if ($appMode === 'development') {
-            $response['source'] = $source;
-            $response['previous'] = $previous;
-        }
-
+        // Use the parent method to handle the exception.
+        parent::handle($exception);
         // Set the status code and echo the response.
-        http_response_code($statusCode);
-        echo json_encode($response);
-
-        // Exit the application.
-        exit;
+        header('Content-Type: application/json');
+        http_response_code($this->response['statusCode']);
+        echo json_encode($this->response);
     }
 }
