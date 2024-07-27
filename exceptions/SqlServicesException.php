b5cd6e8afac78ce9f7ed54eba076c17f9f657e63
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-07-27 21:50:54 +0545

    Created SqlServicesException.

---
+++ exceptions/SqlServicesException.php
@@ -0,0 +1,15 @@
+<?php
+namespace NGFramer\NGFramerPHPExceptions\exceptions;
+
+use NGFramer\NGFramerPHPExceptions\exceptions\supportive\_BaseException;
+use Throwable;
+
+class SqlServicesException extends _BaseException
+{
+    // Constructor of this class.
+    public function __construct($message, $code = 0, ?Throwable $throwable = null, int $statusCode = 500, array $details = [])
+    {
+        // Call the parent constructor for exception.
+        parent::__construct($message, $code, $throwable, $statusCode, $details);
+    }
+}
