125dc82c90f0455becb2b53a92bdc9da5e64d175
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-10-17 17:16:37 +0545

    Created render class centralized to all renderers.

---
+++ Render.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace NGFramer\NGFramerPHPExceptions;
+
+use NGFramer\NGFRamerPHP\config\ApplicationConfig;
+use NGFramer\NGFramerPHPExceptions\renderer\ApiExceptionRenderer;
+use NGFramer\NGFramerPHPExceptions\renderer\CliExceptionRenderer;
+use NGFramer\NGFramerPHPExceptions\renderer\HtmlExceptionRenderer;
+use NGFramer\NGFramerPHPExceptions\renderer\supportive\_BaseRenderer;
+
+class Render
+{
+    /**
+     * Creates and returns an appropriate Exception renderer based on the environment.
+     *
+     * @returns _BaseRenderer
+     */
+    public static function create()
+    {
+        // Check the environment and return the appropriate renderer.
+        // Check if the environment is cli.
+        if (php_sapi_name() === 'cli') {
+            return new CLIExceptionRenderer();
+        }
+
+        // Check if the environment is api.
+        if (ApplicationConfig::get('appType') === 'api') {
+            return new APIExceptionRenderer();
+        }
+
+        // If the environment is not cli or api, return the default renderer.
+        return new HtmlExceptionRenderer();
+    }
+
+}
