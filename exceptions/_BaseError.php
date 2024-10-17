5a0520f9447d332cda9517ce1fd9ea949c24fd31
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-10-17 17:22:38 +0545

    Updated BaseError to a non abstract class.
    The class uses ErrorException and throws the error in exception form (ErrorException) form.

--- exceptions/_BaseError.php
+++ exceptions/_BaseError.php
@@ -4,24 +4,75 @@ namespace NGFramer\NGFramerPHPExceptions\exceptions;
 
 use Error;
 
-abstract class _BaseError extends Error
+class _BaseError extends Error
 {
-    // Function that converts the error to exception.
-    public abstract function convertToException($code, $message, string $file, int $line, array $context = []): void;
-    // How to use this convert to exception to convert error to exception.
-    // The below line of code converts the error to exception.
-    // =====================================================================>
-    // Generate the error details.
-    // $details = self::generateDetails($file, $line, $context);
-    // =====================================================================>
-    // Now throw the error as an exception.
-    // Throw new NameException($message, $code, 0, 500, $details);
+    /**
+     * Function that converts the error to exception.
+     *
+     * @param $code
+     * @param $message
+     * @param string $file
+     * @param int $line
+     * @param array $context
+     * @throws ErrorException
+     */
+    public function convertToException($code, $message, string $file, int $line, array $context = []): void
+    {
+        // Get the type of error.
+        $type = $this->getType($code);
 
-    // How to use this convert to exception to convert error to exception.
-    // The below line of code converts the error to exception.
-    // We use the convertToException function and pass all the error-generated parameters to the function.
-    // And, Use where necessary or include in autoloader to load everywhere.
+        // Generate the error details.
+        $details['errorCode'] = $code;
+        $details['errorType'] = $type;
+        $details['errorMessage'] = $message;
+        $details['errorSource'] = $file . ':' . $line;
 
-    // ================================================================>
-    // set_error_handler([NameError::class, 'convertToException']);
+        // Now prepare and throw an exception.
+        Throw new ErrorException($message, $code, null, 500, $details);
+    }
+
+
+    /**
+     * Function that returns the type of error.
+     *
+     * @param $code
+     * @return string
+     */
+    private function getType($code): string
+    {
+        switch ($code) {
+            case E_ERROR:
+                return 'E_ERROR';
+            case E_WARNING:
+                return 'E_WARNING';
+            case E_PARSE:
+                return 'E_PARSE';
+            case E_NOTICE:
+                return 'E_NOTICE';
+            case E_CORE_ERROR:
+                return 'E_CORE_ERROR';
+            case E_CORE_WARNING:
+                return 'E_CORE_WARNING';
+            case E_COMPILE_ERROR:
+                return 'E_COMPILE_ERROR';
+            case E_COMPILE_WARNING:
+                return 'E_COMPILE_WARNING';
+            case E_USER_ERROR:
+                return 'E_USER_ERROR';
+            case E_USER_WARNING:
+                return 'E_USER_WARNING';
+            case E_USER_NOTICE:
+                return 'E_USER_NOTICE';
+            case E_STRICT:
+                return 'E_STRICT';
+            case E_RECOVERABLE_ERROR:
+                return 'E_RECOVERABLE_ERROR';
+            case E_DEPRECATED:
+                return 'E_DEPRECATED';
+            case E_USER_DEPRECATED:
+                return 'E_USER_DEPRECATED';
+            default:
+                return 'UNKNOWN';
+        }
+    }
 }
